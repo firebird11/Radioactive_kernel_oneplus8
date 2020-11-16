b1449af521c51d7333e75d5a96faf0c46d9de852
LibXZR <xzr467706992@163.com>
2020-11-16 21:27:25 +0800

    touchpanel_common_driver: Use a higher touchpanel sample rate for 60Hz mode
    * This makes it use 180Hz sample rate for 60Hz mode on Oneplus 8T (rather than 120Hz)

--- drivers/oneplus/input/touchscreen/touchpanel_common_driver.c
+++ drivers/oneplus/input/touchscreen/touchpanel_common_driver.c
@@ -5579,7 +5579,7 @@ static int tfb_notifier_callback(struct notifier_block *self, unsigned long even
 			if (event == DRM_PANEL_EARLY_EVENT_BLANK) {
 				mutex_lock(&ts->mutex);
 				if (!ts->is_suspended && (ts->suspend_state == TP_SPEEDUP_RESUME_COMPLETE)) {
-					ts->ts_ops->mode_switch(ts->chip_data, MODE_REFRESH_SWITCH, 0);
+					ts->ts_ops->mode_switch(ts->chip_data, MODE_REFRESH_SWITCH, 1);
 					if (ts->noise_level)
 						ts->ts_ops->mode_switch(ts->chip_data, MODE_GAME, true);
 				}
